c052c409ae694aa402a7f257b1d3ac7455d59a63
Aaron Ang <aaron@ywvision.com>
2021-05-04 10:12:40 +0800

    Create project and install pods.
    Pods installed:
    RxSwift
    RxCocoa
    Alamofire
    NVActivityIndicatorView
    RxBlocking
    RxTest

---
+++ CarTrack-Challenge/CarTrack-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarTrack-Challenge
+//
+//  Created by Aaron Ang on 3/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
